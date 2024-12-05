31cc09a281a3c3d92c5ab986f4a21b3ce6c66b51
w0mb <w0mb@users.noreply.github.com>
2024-12-06 04:41:01 +0700

    saf

--- payBot/webHook.py
+++ payBot/webHook.py
@@ -36,7 +36,7 @@ async def lifespan(app: FastAPI):
 
 # Настройки FastAPI
 app = FastAPI(lifespan=lifespan)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 

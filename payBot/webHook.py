baedef13142a45e5cfb97273a5872190a8f1c729
w0mb <w0mb@users.noreply.github.com>
2024-12-06 04:42:32 +0700

    dfhdfh

--- payBot/webHook.py
+++ payBot/webHook.py
@@ -53,4 +53,4 @@ async def webhook(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8443, ssl_certfile="../sertificates/server.crt", ssl_keyfile="../sertificates/server.key")
+    uvicorn.run(app, host="0.0.0.0", port=8080, ssl_certfile="../sertificates/server.crt", ssl_keyfile="../sertificates/server.key")

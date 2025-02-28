8763a04d0ee4e0a4cf185209d7849cf9ce03846c
w0mb <w0mb@users.noreply.github.com>
2025-02-28 18:55:47 +0700

    add path to utils

--- src_bots/new_privet/utils/file_utils.py
+++ src_bots/new_privet/utils/file_utils.py
@@ -1,15 +1,16 @@
 import os
 
-import os
-
 def remove_string_from_file(string: str, filename: str):
-    if not os.path.exists(filename):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, filename)
+
+    if not os.path.exists(file_path):
         return False
 
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         found = False
         for line in lines:
             if line.strip() == string:
@@ -21,18 +22,22 @@ def remove_string_from_file(string: str, filename: str):
 
 
 async def save_string_to_file(link:str, filename: str) -> str:
-    if not os.path.exists(filename): open(filename, "w", encoding="utf-8").close()
-    with open(filename, "r", encoding="utf-8") as file:
-        if os.path.getsize(filename) != 0:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, filename)
+    if not os.path.exists(file_path): open(file_path, "w", encoding="utf-8").close()
+    with open(file_path, "r", encoding="utf-8") as file:
+        if os.path.getsize(file_path) != 0:
             for line in file:
                 if line.strip() == link: return
-    with open(filename, "a", encoding="utf-8") as file:
+    with open(file_path, "a", encoding="utf-8") as file:
         file.write(link+"\n")
 
 async def get_strings_from_file(filename: str) -> list[str]:
-    if not os.path.exists(filename): open(filename, "w", encoding="utf-8").close()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, filename)
+    if not os.path.exists(file_path): open(file_path, "w", encoding="utf-8").close()
     link_list = []
-    with open(filename, "r", encoding="utf-8") as file:
-        if os.path.getsize(filename) != 0:
+    with open(file_path, "r", encoding="utf-8") as file:
+        if os.path.getsize(file_path) != 0:
             for line in file: link_list.append(line.strip())
             return link_list

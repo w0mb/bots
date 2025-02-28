2ab9cf7fae19be13075bef146c3fe97a866bc4ea
w0mb <w0mb@users.noreply.github.com>
2025-02-28 19:16:52 +0700

    change remove_string_from_file

--- src_bots/new_privet/utils/file_utils.py
+++ src_bots/new_privet/utils/file_utils.py
@@ -1,24 +1,20 @@
 import os
 
-def remove_string_from_file(string: str, filename: str):
+async def remove_string_from_file(string: str, filename: str):
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     file_path = os.path.join(project_root, filename)
 
     if not os.path.exists(file_path):
-        return False
+        return
 
     with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
     with open(file_path, "w", encoding="utf-8") as file:
-        found = False
         for line in lines:
             if line.strip() == string:
-                found = True
                 continue
             file.write(line)
-
-        return found
 
 
 async def save_string_to_file(link:str, filename: str) -> str:

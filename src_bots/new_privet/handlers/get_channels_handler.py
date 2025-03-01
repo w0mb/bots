203c37022014a84d7139fbaddc8e964063737cfc
w0mb <w0mb@users.noreply.github.com>
2025-03-01 22:11:06 +0700

    change path

--- src_bots/new_privet/handlers/get_channels_handler.py
+++ src_bots/new_privet/handlers/get_channels_handler.py
@@ -36,12 +36,12 @@ class GetChannelsHandler(BaseHandler):
                 try:
 
                     channels_list = await get_strings_from_file(f"bot_channel_ids/{bot.id}.txt")
-
+                    print("1")
                     for line in channels_list:
                         parts = line.strip().split(":")
                         if len(parts) >= 3:
                             title, bot_id = parts[1], parts[2]  # bot_id из файла (строка)
-
+                            print("2")
                             # Если bot_id найден в словаре, заменяем его на username
                             bot_username = bot_id_to_username.get(bot_id, f"Unknown({bot_id})")
                             result_dict[title] = bot_username
